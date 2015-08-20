286108f3119ba8389903482f227aad6e96f4d9e2
adica <carmeladi@gmail.com>
2015-08-20 13:32:53 +0300

    remove right click menu + add click event for mobile

--- dist/sarine.viewer.video.js
+++ dist/sarine.viewer.video.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.video - v0.2.0 -  Wednesday, August 19th, 2015, 2:31:19 PM 
+sarine.viewer.video - v0.2.0 -  Thursday, August 20th, 2015, 1:31:35 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -69,6 +69,12 @@ sarine.viewer.video - v0.2.0 -  Wednesday, August 19th, 2015, 2:31:19 PM
           poster: img.src
         });
         return defer.resolve(this);
+      });
+      this.element.find("video").bind('contextmenu', function() {
+        return false;
+      });
+      this.element.find("video").on('click', function() {
+        return this.play();
       });
       return defer;
     };
