ed00dbb23aa9b7c055c3379ba17118af8cefb08b
ronishoham <roni.shoham@sarine.com>
2018-01-11 09:44:59 +0200

    feature 107381

--- GruntfileBundle.js
+++ GruntfileBundle.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     var target = grunt.option('target') || "";
     grunt
     grunt.initConfig({
-        config: grunt.file.readJSON(target + "bower.json"),
+        config: grunt.file.readJSON(target + "package.json"),
         clean: {
             build: [target + "lib/", target + "dist/", target + "build/"],
             bundlecoffee: [target + "coffee/*.bundle.coffee"],
@@ -100,6 +100,12 @@ module.exports = function(grunt) {
                 src: [target + 'coffee/<%= config.name %>.bundle.coffee']
 
             }
+        },
+        copy: {
+            bundle: {
+                dest: target + 'dist/<%= config.name %>.config',
+                src: [target + '<%= config.name %>.config']
+            }
         }
     })
     grunt.registerTask('bundle', [
@@ -114,7 +120,9 @@ module.exports = function(grunt) {
         'concat:coffee',
         'coffee:build',
         'uglify',
-        'clean:postbuild'
+        'clean:postbuild',
+        'copyVersion',
+        'copy:bundle'
     ]);
     grunt.registerMultiTask('commentsCoffee', 'Remove comments from production code', function() {
         this.files[0].src.forEach(function(file) {
@@ -127,5 +135,18 @@ module.exports = function(grunt) {
             }
             grunt.file.write(file, contents);
         });
+    });
+    grunt.registerTask('copyVersion' , 'copy version from package.json to sarine.viewer.clarity.config' , function (){
+        var packageFile = grunt.file.readJSON(target + 'package.json');
+        var configFileName = target + packageFile.name + '.config';
+        var copyFile = null;
+        if (grunt.file.exists(configFileName))
+            copyFile = grunt.file.readJSON(configFileName);
+        
+        if (copyFile == null)
+            copyFile = {};
+
+        copyFile.version = packageFile.version;
+        grunt.file.write(configFileName , JSON.stringify(copyFile));
     });
 };
